0e7a5bd927c849ebb957768a7870c7b6ef24ad97
UwUssimo Robinson <uwussimo@icloud.com>
2021-10-12 00:32:25 +0500

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello world!";
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 8080 });
