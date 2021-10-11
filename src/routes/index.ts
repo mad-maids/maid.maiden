78ce02371a8175fc96dad93a01f9da65687b4ade
UwUssimo Robinson <uwussimo@icloud.com>
2021-10-12 03:44:56 +0500

    body for index page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,6 +9,13 @@ import {
 
 const router = new Router();
 
+router.get('/', async (ctx) => {
+  ctx.body = {
+    intranet: 'http://api.n.maid.uz/intranet/',
+    timetable: 'http://api.n.maid.uz/timetable/',
+  };
+});
+
 router.get('/timetable', async (ctx) => {
   const types = await getGroups('timetable');
   ctx.body = {
