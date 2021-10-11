db7a497b74561cc1385d1c260ecb97c66274137e
UwUssimo Robinson <uwussimo@icloud.com>
2021-10-12 04:52:32 +0500

    telegram added

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
 import path from 'path';
 
+// Telegram Bot
+export const env = process.env
+
+// API Assets
 export const timetableDirectory = (type: string) =>
   path.join(__dirname, '../assets/timetable', type);
 export const intranetDirectory = (type: string) =>
