a976afd2e60f370604965d4e9384e95535c3c0d1
UwUssimo Robinson <uwussimo@icloud.com>
2021-10-12 16:53:05 +0500

    doc summary

--- api/health.ts
+++ api/health.ts
@@ -1,20 +1,10 @@
 import type { APIHandler } from 'aleph/types.d.ts'
 
-const deno = () => {
-  fetch('http://api.d.maid.uz').then(response => {
-    if (response.status === 200) {
-      return 200
-    }
-  })
-}
-const node = () => {
-  fetch('http://api.n.maid.uz').then(response => {
-    if (response.status === 200) {
-      return 200
-    }
-  })
+const check = async (link: string) => {
+  const req = await fetch('http://api.d.maid.uz').then(req => req.status)
+  return req
 }
 
 export const handler: APIHandler = ({ response }) => {
-  response.json({ d: deno(), n: node() })
+  response.json({ d: check('http://api.d.maid.uz'), n: check('http://api.n.maid.uz') })
 }
