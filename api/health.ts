523784500566dddc84e3a8333cd74ee76b28009d
UwUssimo Robinson <uwussimo@icloud.com>
2021-10-12 16:41:11 +0500

    Update health.ts

--- api/health.ts
+++ api/health.ts
@@ -1,19 +1,20 @@
 import type { APIHandler } from 'aleph/types.d.ts'
 
+const deno = () => {
+  fetch('http://api.d.maid.uz').then(response => {
+    if (response.status === 200) {
+      return 200
+    }
+  })
+}
+const node = () => {
+  fetch('http://api.n.maid.uz').then(response => {
+    if (response.status === 200) {
+      return 200
+    }
+  })
+}
+
 export const handler: APIHandler = ({ response }) => {
-  let deno = () => {
-    fetch('http://api.d.maid.uz').then(response => {
-      if (response.status === 200) {
-        return 200
-      }
-    })
-  }
-  let node = () => {
-    fetch('http://api.n.maid.uz').then(response => {
-      if (response.status === 200) {
-        return 200
-      }
-    })
-  }
   response.json({ d: deno(), n: node() })
 }
